c9d0c970a49d264aeab90a5df4cc83d8f3195936
sydni wright <sydniw@udel.edu>
2024-06-10 16:28:35 -0400

    added contact and project pages, tentative nav bar

---
+++ website/src/pages/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export function Contact(){
+    return(
+        <Container className="contact">
+            <h1>Contact Information</h1>
+            <h3>Personal Email: wrightsydni6@gmail.com</h3>
+            <h3>School Email: sydniw@udel.edu</h3>
+            <h3>Phone: 302-766-5267</h3>
+            <h3><a href="https://www.linkedin.com/in/sydni-wright-01ab82225/">LinkedIn</a></h3>
+        </Container>
+    );
+}
---
+++ website/src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+export function Projects(){
+    return(
+        <Container className="projects">
+            <h1>Project Collection</h1>
+        </Container>
+    );
+}
